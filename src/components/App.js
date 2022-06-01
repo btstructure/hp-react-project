83ba5cae5a14bf0000736cc69791f3e664ddb3b9
Ana Zitzow <anasempai89@gmail.com>
2022-06-01 09:43:38 -0700

    NavBar working, and all components have been formatted

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import "../App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import NavBar from "./NavBar"
-import CharacterContainer from "./CharacterContainer"
+import NavBar from "./NavBar";
+import CharacterList from "./CharacterList";
+import NewCharacterForm from "./NewCharacterForm";
 
 function App() {
-  const [ page, setPage ] = useState("/");
+  const [page, setPage] = useState("/");
 
   return (
     <div className="App">
       <NavBar onChangePage={setPage} />
-        <Routes>
-          <Route path="/characters" element={<Home />}/>
-          <Route
-          path="/newCharacter" element={<CharacterContainer />}/>
-        </Routes>
+      <Routes>
+        <Route path="/characters" element={<CharacterList />} />
+        <Route path="/newCharacter" element={<NewCharacterForm />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar(){
-
-return (
-  <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/characters">Characters</NavLink>
-    <NavLink to="/newCharacter">Add New Character</NavLink>
-  </nav>
-);
+function NavBar() {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/characters">Characters</NavLink>
+      <NavLink to="/newCharacter">Add New Character</NavLink>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
